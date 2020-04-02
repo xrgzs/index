0572151a83def72f59a89b1cebb1bbc12564a022
Xiaoran Studio <dalizxr@qq.com>
2020-04-02 15:16:25 +0800

    Update xrsys.bat

--- xrsys.bat
+++ xrsys.bat
@@ -1,6 +1,6 @@
 bitsadmin /transfer ar http://ys-d.ys168.com/609605302/r712K553I5PHIVHSvhJ/aria2c.exe %dir%\aria2c.exe
-aria2c.exe http://ys-d.ys168.com/609605327/SITwfGs712K553I6LLJO/7za.exe
-aria2c.exe -i http://ys-d.ys168.com/609605322/q635F286I26N61jTulKu/IQIlist.txt
+aria2c.exe "http://ys-d.ys168.com/609605327/SITwfGs712K553I6LLJO/7za.exe"
+aria2c.exe -i "http://ys-d.ys168.com/609605322/q635F286I26N61jTulKu/IQIlist.txt"
 pause
 7za.exe x IQI9_v9.1008_x64_x86_“—‘§÷√∆Ù∂Ø.7z.001
 pause
