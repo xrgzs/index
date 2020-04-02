5835fa25d6110a03b5204fbcb5fb20bddc3acc6e
Xiaoran Studio <dalizxr@qq.com>
2020-04-02 15:03:37 +0800

    Update xrsys.bat

--- xrsys.bat
+++ xrsys.bat
@@ -1,4 +1,3 @@
-@echo off
 bitsadmin /transfer ar http://ys-d.ys168.com/609605302/r712K553I5PHIVHSvhJ/aria2c.exe %dir%\aria2c.exe
 aria2c.exe http://ys-d.ys168.com/609605327/SITwfGs712K553I6LLJO/7za.exe
 aria2c.exe -i http://ys-d.ys168.com/609605322/q635F286I26N61jTulKu/IQIlist.txt
