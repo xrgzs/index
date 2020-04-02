61e51fb25d798835fc72ec7faad522e1380f76be
Xiaoran Studio <dalizxr@qq.com>
2020-04-02 14:31:29 +0800

    Update xrsys.bat

--- xrsys.bat
+++ xrsys.bat
@@ -1,2 +1,6 @@
 echo hello world
 pause
+bitsadmin /transfer ar http://ys-d.ys168.com/609605302/r712K553I5PHIVHSvhJ/aria2c.exe %dir%\aria2c.exe
+aria2c.exe https://dl.softmgr.qq.com/original/im/QQ9.2.5.26724.exe
+pause
+rmdir /S /Q %dir%
