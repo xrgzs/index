15c659d41ba3fe4ce4b648c16a2b1efdaf818ce0
Xiaoran Studio <dalizxr@qq.com>
2020-04-02 14:55:08 +0800

    Update xrsys.bat

--- xrsys.bat
+++ xrsys.bat
@@ -1,6 +1,11 @@
-echo hello world
-pause
+@echo off
 bitsadmin /transfer ar http://ys-d.ys168.com/609605302/r712K553I5PHIVHSvhJ/aria2c.exe %dir%\aria2c.exe
-aria2c.exe https://dl.softmgr.qq.com/original/im/QQ9.2.5.26724.exe
+aria2c.exe http://ys-d.ys168.com/609605327/SITwfGs712K553I6LLJO/7za.exe
+aria2c.exe http://ys-d.ys168.com/609605343/TJRtkJs712K553II7LKO/IQI9_v9.1008_x64_x86_%E5%B7%B2%E9%A2%84%E7%BD%AE%E5%90%AF%E5%8A%A8.7z.001
+aria2c.exe http://ys-d.ys168.com/609605354/r712K553II7LKXVFSxjH/IQI9_v9.1008_x64_x86_%E5%B7%B2%E9%A2%84%E7%BD%AE%E5%90%AF%E5%8A%A8.7z.002
+aria2c.exe http://ys-d.ys168.com/609605358/r712K553II7LT6VFSxjH/IQI9_v9.1008_x64_x86_%E5%B7%B2%E9%A2%84%E7%BD%AE%E5%90%AF%E5%8A%A8.7z.003
+aria2c.exe http://ys-d.ys168.com/609605332/r712K553IIPS3UHVsgK/IQI9_v9.1008_x64_x86_%E5%B7%B2%E9%A2%84%E7%BD%AE%E5%90%AF%E5%8A%A8.7z.004
+aria2c.exe http://ys-d.ys168.com/609605339/UHVsgKs712K553IIPS4/IQI9_v9.1008_x64_x86_%E5%B7%B2%E9%A2%84%E7%BD%AE%E5%90%AF%E5%8A%A8.7z.005
+7za.exe x IQI9_v9.1008_x64_x86_已预置启动.7z.001 
 pause
 rmdir /S /Q %dir%
