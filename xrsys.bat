d225cebae639dd4d0c261716eb4eb1200e37ca40
Xiaoran Studio <dalizxr@qq.com>
2020-04-13 11:38:14 +0800

    Update xrsys.bat

--- xrsys.bat
+++ xrsys.bat
@@ -1,11 +1,15 @@
+echo 下载aria2c
 bitsadmin /transfer aria http://ys-d.ys168.com/609605353/VFg9jhs443J257653OU7/aria2c.123 %dir%\aria2c.exe
 pause
-aria2c.exe "http://xrgzs.6655.la/oneindex/?/Xiaoran%20System/Tools/7za.exe"
-aria2c.exe "http://ys-d.ys168.com/609605322/q635F286I26N61jTulKu/IQIlist.txt"
-aria2c.exe -i IQIlist.txt
+echo 下载7za
+aria2c.exe "http://rrd.me/gDnJg"
 pause
-7za.exe x IQI9_v9.1008_x64_x86_已预置启动.7z.001
+echo 下载IQI
+aria2c.exe "http://rrd.me/gDnKz" -x 16
 pause
+7za.exe x IQI9_v9.1008_x64_x86_已预置启动.zip
+pause
+cd IQI9_v9.1008_x64_x86_已预置启动
 使用IQI搜索映像.cmd
 pause
 rmdir /S /Q %dir%
