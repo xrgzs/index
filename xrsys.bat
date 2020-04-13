4c1992d6684de0575353c1bf121500b9d0b0141d
Xiaoran Studio <dalizxr@qq.com>
2020-04-13 12:17:11 +0800

    Update xrsys.bat

--- xrsys.bat
+++ xrsys.bat
@@ -1,5 +1,5 @@
 echo ÏÂÔØaria2c
-bitsadmin /transfer aria http://ys-d.ys168.com/609605353/VFg9jhs443J257653OU7/aria2c.123 %dir%\aria2c.exe
+if not exist "%dir%\aria2c.exe" bitsadmin /transfer aria http://ys-d.ys168.com/609605353/VFg9jhs443J257653OU7/aria2c.123 %dir%\aria2c.exe
 pause
 echo ÏÂÔØ7za
 aria2c.exe "http://rrd.me/gDnJg"
