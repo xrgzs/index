b7156240ffb6e7b528fa4750f278a192c8bf8ad5
Xiaoran Studio <dalizxr@qq.com>
2020-04-13 10:09:21 +0800

    Update xrsys.bat

--- xrsys.bat
+++ xrsys.bat
@@ -1,6 +1,6 @@
-bitsadmin /transfer aria http://ys-d.ys168.com/609605302/r712K553I5PHIVHSvhJ/aria2c.exe %dir%\aria2c.exe
+bitsadmin /transfer aria http://xrgzs.6655.la/oneindex/?/Xiaoran%20System/Tools/aria2c.exe %dir%\aria2c.exe
 pause
-aria2c.exe "http://ys-d.ys168.com/609605327/SITwfGs712K553I6LLJO/7za.exe"
+aria2c.exe "http://xrgzs.6655.la/oneindex/?/Xiaoran%20System/Tools/7za.exe"
 aria2c.exe "http://ys-d.ys168.com/609605322/q635F286I26N61jTulKu/IQIlist.txt"
 aria2c.exe -i IQIlist.txt
 pause
