4a24e10e709ebb0bac696a21de1080820c793772
Xiaoran Studio <dalizxr@qq.com>
2020-06-20 21:47:13 +0800

    Delete xrsys.bat

--- xrsys.bat
+++
@@ -1,5 +0,0 @@
-if not exist "%dir%\aria2c.exe" bitsadmin /transfer aria http://ys-d.ys168.com/609605353/VFg9jhs443J257653OU7/aria2c.123 %dir%\aria2c.exe
-pause
-aria2c.exe "http://rrd.me/gDpkg"
-pause
-program.bat
